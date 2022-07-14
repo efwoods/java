94bc9e8dfaf5acbd2901c991f72be63eaa16faac
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-14 10:41:10 -0400

    completed coding challenge: calculate employee salary

--- SalaryCalculatorChallenge/src/EmployeeSalary.java
+++ SalaryCalculatorChallenge/src/EmployeeSalary.java
@@ -1,25 +1,31 @@
 import java.util.Scanner;
 
 public class EmployeeSalary {
-    public static int calculateEmployeeSalary(){
-        int hoursWorkedPerWeek;
-        int moneyMadePerHour;
+    public static int calculateEmployeeSalary(int hoursWorkedPerWeek,
+                                              int moneyMadePerHour,
+                                              int vacationDays){
         int grossYearlySalary;
-        Scanner answer = new Scanner(System.in);
 
-
-        System.out.println("How many hours does the employee work per week?");
-        hoursWorkedPerWeek = answer.nextInt();
-        System.out.println("How much does the employee make per hour?");
-        moneyMadePerHour = answer.nextInt();
-        grossYearlySalary = hoursWorkedPerWeek * moneyMadePerHour * 52;
+        grossYearlySalary = (hoursWorkedPerWeek * moneyMadePerHour * 52) - (vacationDays * moneyMadePerHour * 8);
         return grossYearlySalary;
     }
 
-     public static int main(String[] args){
-        int grossYearlySalary = calculateEmployeeSalary();
-        System.out.println("The employee's yearly salary is: $" + grossYearlySalary);
+     public static void main(String[] args){
+         int hoursWorkedPerWeek;
+         int moneyMadePerHour;
+         int grossYearlySalary;
+         int vacationDays;
+         Scanner answer = new Scanner(System.in);
 
+         System.out.println("How many hours does the employee work per week?");
+         hoursWorkedPerWeek = answer.nextInt();
+         System.out.println("How much does the employee make per hour?");
+         moneyMadePerHour = answer.nextInt();
+         System.out.println("How many days of vacation has the employee taken?");
+         vacationDays = answer.nextInt();
+
+        grossYearlySalary = calculateEmployeeSalary(hoursWorkedPerWeek, moneyMadePerHour, vacationDays);
+        System.out.println("The employee's yearly salary is: $" + grossYearlySalary);
     }
 
 
