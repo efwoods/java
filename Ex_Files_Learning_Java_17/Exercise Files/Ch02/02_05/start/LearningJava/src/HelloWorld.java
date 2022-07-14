88d71483c0e5b562cd166a53628e1cf5c235fe36
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-14 12:35:59 -0400

    adding java datastructures submodule

--- Ex_Files_Learning_Java_17/Exercise Files/Ch02/02_05/start/LearningJava/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        double studentGPA = 3.45;
-        String studentFirstName = "Kayla";
-        String studentLastName = "Harley";
-    }
-}
--- Ex_Files_Learning_Java_17/Exercise Files/Ch05/05_06/end/LearningJava/src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        double result = Math.pow(2, 5);
-        System.out.println(result);
-    }
-}
