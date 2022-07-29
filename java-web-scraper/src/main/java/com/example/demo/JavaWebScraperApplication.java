1aafc355ff3f6c431142114af4c65f84a9a39010
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-29 19:41:01 -0400

    commit

---
+++ java-web-scraper/src/main/java/com/example/demo/JavaWebScraperApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class JavaWebScraperApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebScraperApplication.class, args);
+	}
+
+}
---
+++ java-web-scraper/src/main/java/com/example/demo/entities/Greeting.java
@@ -0,0 +1,16 @@
+package com.example.demo.entities;
+
+public class Greeting {
+    private String message;
+
+    public Greeting(String format) {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
